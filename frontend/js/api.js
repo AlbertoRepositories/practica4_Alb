ce8d88dff2864b609c8ff392c48e0e2edd88a08d
Albeato <albertoemmanueltinajerovelazco@gmail.com>
2026-04-24 22:19:38 -0600

    connect frontend to render backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,8 +2,14 @@
 export async function getProducts(page = 1, limit = 4) {
   try {
     // Uso de fetch para cargar los productos y la paginación con un límite de productos por página en el enlace local del servidor
-    // para la página
-    const response = await fetch(`http://localhost:3100/api/products?page=${page}&limit=${limit}`);
+    // para la página, LOCAL, directo en VS con npm run dev tras install express y con validación para si lo hace con el
+    // acceso público en línea:
+    const BASE_URL =
+      window.location.hostname === "localhost"
+      ? "http://localhost:3100"
+      : "https://practica4-alb.onrender.com";
+
+    const response = await fetch(`${BASE_URL}/api/products?page=${page}&limit=${limit}`);
 
     if (!response.ok) { // Mensaje de error si no hay respuesta
       throw new Error("Error al obtener productos");
